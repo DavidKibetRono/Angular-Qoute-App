f38fbdee0913f7edb04d8224d58e9f935aeb2049
DavidKibetRono <david.rono@student.moringaschool.com>
2021-12-27 18:34:23 +0300

    add the code logic for the form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter, Input } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+ 
+
+@Output() frmData:any=new EventEmitter()
 
   constructor() { }
-
+quotes=new Quote("","","",new Date())
   ngOnInit(): void {
+
   }
+
+
+  formDetails(data:any){
+    let dataFromForm=new Quote(data.value.name,data.value.mainQuote,data.value.author,data.value.datePosted)
+    this.frmData.emit(dataFromForm)
+    data.reset()
+  }
+
 
 }
