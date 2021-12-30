c8623ef778eb62e44110457451ad61d3954b3a72
DavidKibetRono <david.rono@student.moringaschool.com>
2021-12-30 19:48:01 +0300

    create an array and method to store form data

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Quote } from '../quote'; 
+
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  public count = 0;
+
+ @Input() theQuotes!:Quote 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  quotes:Quote[]=[
+    new Quote("Kibet","Don't confuse patience and wasting time please","Mark Kurt",new Date(2021,12,17)),
+    new Quote("Mercy","Better late than never","Antonio Rudiger",new Date(2020,12,17))
+    ]
+
+    myFormData(data:any){
+      data.datePosted=new Date(data.datePosted)
+      this.quotes.push(data)
+      // data.reset()
+        }
+
+
+
+
+}
