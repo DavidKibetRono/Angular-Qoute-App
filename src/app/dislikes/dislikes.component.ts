d868e067d8942fa0a7a0e8bb796a75e687519b65
DavidKibetRono <david.rono@student.moringaschool.com>
2021-12-30 19:33:03 +0300

    create a method responsible for counting the number of downvotes

---
+++ src/app/dislikes/dislikes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dislikes',
+  templateUrl: './dislikes.component.html',
+  styleUrls: ['./dislikes.component.css']
+})
+export class DislikesComponent implements OnInit {
+  downVoteNumber:number=0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  counntDownvote(){
+    this.downVoteNumber++
+  }
+}
