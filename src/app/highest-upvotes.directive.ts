72abc85f085e7ef2495786d91259a82c106cb79d
DavidKibetRono <david.rono@student.moringaschool.com>
2021-12-30 19:53:43 +0300

    add a custom directive that highlights the quote with the highest upvotes

---
+++ src/app/highest-upvotes.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighestUpvotes]'
+})
+export class HighestUpvotesDirective {
+  constructor(private elem: ElementRef) {
+    this.elem.nativeElement.style.backgroundColor = 'lightblue';
+    this.elem.nativeElement.style.border = '1px solid black';
+
+   }
+
+}
+
+
